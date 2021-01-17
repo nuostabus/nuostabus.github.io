76318feace69b34212b3200897f9792e2d8120da
nuostabus <77547212+nuostabus@users.noreply.github.com>
2021-01-17 15:44:51 +0200

    my first commit

---
+++ app.js
@@ -0,0 +1,158 @@
+const alanQuotes = [
+  '"You didn’t come into this world. You came out of it, like a wave from the ocean. You are not a stranger here."',
+  '"The more we struggle for life (as pleasure), the more we are actually killing what we love."',
+  '"The meaning of life is just to be alive. It is so plain and so obvious and so simple. And yet, everybody rushes around in a great panic as if it were necessary to achieve something beyond themselves."',
+  '"To put is still more plainly: the desire for security and the feeling of insecurity are the same thing. To hold your breath is to lose your breath."',
+  '"You are a function of what the whole universe is doing in the same way that a wave is a function of what the whole ocean is doing."',
+  '“There will always be suffering. But we must not suffer over the suffering.”',
+  '“The only way to make sense out of change is to plunge into it, move with it, and join the dance.”',
+  '“Try to imagine what it will be like to go to sleep and never wake up… now try to imagine what it was like to wake up having never gone to sleep.”',
+  '“Only words and conventions can isolate us from the entirely undefinable something which is everything.”',
+  '“Meditation is the discovery that the point of life is always arrived at in the immediate moment.”',
+  '“Everyone has love, but it can only come out when he is convinced of the impossibility and the frustration of trying to love himself.”',
+  '“Life and love generate effort, but effort will not generate them. Faith in life, in other people, and in oneself, is the attitude of allowing the spontaneous to be spontaneous, in its own way and in its own time.”',
+  '“The meaning of life is just to be alive. It is so plain and so obvious and so simple. And yet, everybody rushes around in a great panic as if it were necessary to achieve something beyond themselves.”',
+  '“Problems that remain persistently insoluble should always be suspected as questions asked in the wrong way.”',
+  '“To put is still more plainly: the desire for security and the feeling of insecurity are the same thing. To hold your breath is to lose your breath.”',
+  '“We seldom realize, for example, that our most private thoughts and emotions are not actually our own. For we think in terms of languages and images which we did not invent, but which were given to us by our society.”',
+  '“The greater part of human activity is designed to make permanent those experiences and joys which are only lovable because they are changing.”',
+  '“Things are as they are. Looking out into it the universe at night, we make no comparisons between right and wrong stars, nor between well and badly arranged constellations.”',
+  '"You are an aperture through which the universe is looking at and exploring itself."',
+  '"Every intelligent individual wants to know what makes him tick, and yet is at once fascinated and frustrated by the fact that oneself is the most difficult of all things to know."',
+  '"One is a great deal less anxious if one feels perfectly free to be anxious, and the same may be said of guilt."',
+  '"Normally, we do not so much look at things as overlook them."',
+  '“Man suffers only because he takes seriously what the gods made for fun.”',
+  '“This is the real secret of life — to be completely engaged with what you are doing in the here and now. And instead of calling it work, realize it is play.”',
+  '“You see, for all life is an act of faith and an act of gamble. The moment you take a step, you do so on an act of faith because you don’t really know that the floor’s not going to give under your feet. The moment you take a journey, what an act of faith. The moment that you enter into any kind of human undertaking in relationship, what an act of faith.”',
+  '“If we cling to belief in God, we cannot likewise have faith, since faith is not clinging but letting go.”',
+  '“Without birth and death, and without the perpetual transmutation of all the forms of life, the world would be static, rhythm-less, undancing, mummified.”',
+  '“Through our eyes, the universe is perceiving itself. Through our ears, the universe is listening to its harmonies. We are the witnesses through which the universe becomes conscious of its glory, of its magnificence.”',
+  '“When you die, you don’t have to deal with everlasting nonexistence because that’s not an experience.”',
+  '“Other people teach us who we are. Their attitudes to us are the mirror in which we learn to see ourselves, but the mirror is distorted. We are, perhaps, rather dimly aware of the immense power of our social environment.”',
+  '“Life is like music for its own sake. We are living in an eternal now, and when we listen to music we are not listening to the past, we are not listening to the future, we are listening to an expanded present.”',
+  '“You do not play a sonata in order to reach the final chord, and if the meanings of things were simply in ends, composers would write nothing but finales.”',
+  '“To remain stable is to refrain from trying to separate yourself from a pain because you know that you cannot. Running away from fear is fear, fighting pain is pain, trying to be brave is being scared. If the mind is in pain, the mind is pain. The thinker has no other form than his thought. There is no escape.”',
+  '“What we have forgotten is that thoughts and words are conventions, and that it is fatal to take conventions too seriously. A convention is a social convenience, as, for example, money … but it is absurd to take money too seriously, to confuse it with real wealth … In somewhat the same way, thoughts, ideas and words are “coins” for real things.”',
+  '“I have no other self than the totality of things of which I am aware.”',
+  '“To travel is to be alive, but to get somewhere is to be dead, for as our own proverb says, “To travel well is better than to arrive.”',
+  '“A world which increasingly consists of destinations without journeys between them, a world which values only “getting somewhere” as fast as possible, becomes a world without substance. One can get anywhere and everywhere, and yet the more this is possible, the less is anywhere and everywhere worth getting to.”',
+];
+
+const yuvalQuotes = [
+  '“Morality doesn’t mean ‘following divine commands’. It means ‘reducing suffering’. Hence in order to act morally, you don’t need to believe in any myth or story. You just need to develop a deep appreciation of suffering.”',
+  '“In a world deluged by irrelevant information, clarity is power.”',
+  '“Humans think in stories rather than in facts, numbers, or equations, and the simpler the story, the better.”',
+  '“At present, people are happy to give away their most valuable asset—their personal data—in exchange for free email services and funny cat videos. It’s a bit like African and Native American tribes who unwittingly sold entire countries to European imperialists in exchange for colorful beads and cheap trinkets.”',
+  '“One of the greatest fictions of all is to deny the complexity of the world and think in absolute terms.”',
+  '"Individual humans know embarrassingly little about the world, and as history has progressed, they have come to know less and less. A hunter-gatherer in the Stone Age knew how to make her own clothes, how to start a fire, how to hunt rabbits, and how to escape lions. We think we know far more today, but as individuals, we actually know far less. We rely on the expertise of others for almost all our needs."',
+  '“Homo sapiens is just not built for satisfaction. Human happiness depends less on objective condition and more on our own expectations. Expectations, however, tend to adapt to conditions, including to the condition of other people. When things improve, expectations balloon, and consequently even dramatic improvement in conditions might leave us as dissatisfied as before.”',
+  '“If you cannot afford to waste time, you will never find the truth.”',
+  '“The first thing you need to know about yourself is that you are not a story.”',
+  '“The danger is that if we invest too much in developing AI and too little in developing human consciousness, the very sophisticated artificial intelligence of computers might only serve to empower the natural stupidity of humans.”',
+  '“If you really want truth, you need to escape the black hole of power and allow yourself to waste a lot of time wandering here and there on the periphery. Revolutionary knowledge rarely makes it to the center, because the center is built on existing knowledge”',
+  '“Change Is the Only Constant”',
+  '“When a thousand people believe some made-up story for one month, that\'s fake news. When a billion people believe it for a thousand years, that\'s a religion, and we are admonished not to call it "fake news" in order not to hurt the feelings of the faithful (or incur their wrath).”',
+  '“Whenever politicians start talking in mystical terms, beware. They might be trying to disguise and excuse real suffering by wrapping it up in big incomprehensible words. Be particularly careful about the following four words: sacrifice, eternity, purity, redemption. If you hear any of these, sound the alarm. And if you happen to live in a country whose leader routinely says things like ‘Their sacrifice will redeem the purity of our eternal nation’ – know that you are in deep trouble.”',
+  '“Technology isn’t bad. If you know what you want in life, technology can help you get it. But if you don’t know what you want in life, it will be all too easy for technology to shape your aims for you and take control of your life. Especially as technology gets better at understanding humans, you might increasingly find yourself serving it, instead of it serving you.”',
+  '“How do you cause people to believe in an imagined order such as Christianity, democracy or capitalism? First, you never admit that the order is imagined.”',
+  '“History is something that very few people have been doing while everyone else was ploughing fields and carrying water buckets.”',
+  '“Money is the most universal and most efficient system of mutual trust ever devised.”',
+  '“Happiness does not really depend on objective conditions of either wealth, health or even community. Rather, it depends on the correlation between objective conditions and subjective expectations.”',
+  '“We do not become satisfied by leading a peaceful and prosperous existence. Rather, we become satisfied when reality matches our expectations. The bad news is that as conditions improve, expectations balloon.”',
+  '“The most common reaction of the human mind to achievement is not satisfaction, but craving for more.”',
+  '“Large numbers of strangers can cooperate successfully by believing in common myths. Any large-scale human cooperation – whether a modern state, a medieval church, an ancient city or an archaic tribe – is rooted in common myths that exist only in people’s collective imagination.”',
+  '"History isn’t a single narrative, but thousands of alternative narratives. Whenever we choose to tell one, we are also choosing to silence others."',
+  '“One of the greatest fictions of all is to deny the complexity of the world and think in absolute terms:”',
+];
+
+const allQuotes = alanQuotes.concat(yuvalQuotes);
+
+const quoteButton = document.getElementById('quote-btn');
+
+let showedQuotes = [];
+
+let leftUnique = [];
+
+let authorQuotes; //placeholder to replace
+
+
+//if everything is finished you need to show an alert "you've seen all quotes, do you want to reset?, click this button if yes" 
+
+const generateQuote = () => {
+  if (showedQuotes.length < 1) {
+    randomElement = authorQuotes[Math.floor(Math.random() * authorQuotes.length)]; //need to replace "alan with selected variable"
+    showedQuotes.push(randomElement);
+    document.getElementById('quotes').innerHTML = randomElement;
+    leftUnique = authorQuotes.filter(function (el) {
+      return showedQuotes.indexOf(el) < 0; //checks for unique values which are left
+    });
+    console.log(randomElement, showedQuotes, leftUnique);
+  } 
+  else {
+    if (leftUnique.length == 0) {
+      alert('You have seen all quotes, so it will start from the beginning');
+      showedQuotes = [];
+    } else {
+      newRandomElement =
+      leftUnique[Math.floor(Math.random() * leftUnique.length)];
+      showedQuotes.push(newRandomElement);
+      leftUnique = authorQuotes.filter(function (el) {
+        return showedQuotes.indexOf(el) < 0; //checks for unique values which are left
+      });
+      console.log(newRandomElement, leftUnique);
+      document.getElementById('quotes').innerHTML = newRandomElement;
+    }
+  }
+};
+
+/* const generateQuote = () => {
+  randomElement = authorQuotes[Math.floor(Math.random() * authorQuotes.length)]; //need to replace "alan with selected variable"
+  if (showedQuotes.length < 1) {
+    showedQuotes.push(randomElement);
+    console.log(randomElement, showedQuotes);
+    document.getElementById("quotes").innerHTML = randomElement;
+  } else {
+    if (showedQuotes.length === authorQuotes.length) {
+      alert('You have seen all quotes, so it will start from the beginning');
+      showedQuotes = [];
+    } else {
+    let leftUnique = authorQuotes.filter(function (el) {
+      //checks for unique values which are left
+      return showedQuotes.indexOf(el) < 0;
+    });
+    newRandomElement =
+      leftUnique[Math.floor(Math.random() * leftUnique.length)];
+    showedQuotes.push(newRandomElement);
+    console.log('the same', newRandomElement, leftUnique);
+    document.getElementById("quotes").innerHTML = newRandomElement;
+  }};
+};
+ */
+
+
+let selectedAutor =  () => {
+ let selectedValue = document.getElementById("author").value;
+ console.log(selectedValue);
+ if (selectedValue == "Yuval"){
+  authorQuotes = yuvalQuotes;
+   console.log(authorQuotes);
+    }  if (selectedValue == "Alan") {
+      authorQuotes = alanQuotes;
+      console.log(authorQuotes);
+ } if (selectedValue == "All") {
+  authorQuotes = allQuotes;
+  console.log(authorQuotes);
+ }
+};
+
+selectedAutor(); //if selectedValue=Alan, then Alan quotes, if not, other quotes, if "all" then all, if none, then all
+
+
+quoteButton.addEventListener('click', generateQuote);
+
+function testFunction() {
+  selectedAutor();
+  generateQuote();
+};
+
+
